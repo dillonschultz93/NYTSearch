aebf558a5fe4a5040c312a671b4a681cd09a2651
klanza <klanza91@gmail.com>
2017-11-30 20:02:46 -0700

    Created beginning ajax query/URL build

---
+++ assets/javascript/logic.js
@@ -0,0 +1,22 @@
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+    //Standard API key
+    'api-key': "3af329218907494292b4799b9106fbdd",
+    //Search term
+    'q': "Net neutrality",
+    //Dates of search, format YYYYMMDD
+    'begin_date': "20170101",
+    'end_date': "20170601",
+    //sort method, string, newest or oldest
+    'sort': "newest",
+    //True or false, highlights search paramater
+    'hl': "true"
+});
+$.ajax({
+    url: url,
+    method: 'GET',
+}).done(function (result) {
+    console.log(result);
+}).fail(function (err) {
+    throw err;
+});
