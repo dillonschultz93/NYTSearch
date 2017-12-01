20196f58c6aea06043343f4477ea41d3df8cefc7
klanza <klanza91@gmail.com>
2017-11-30 20:37:16 -0700

    Added globally scoped variables

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,17 +1,30 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-    //Standard API key
-    'api-key': "3af329218907494292b4799b9106fbdd",
-    //Search term
-    'q': "Net neutrality",
-    //Dates of search, format YYYYMMDD
-    'begin_date': "20170101",
-    'end_date': "20170601",
-    //sort method, string, newest or oldest
-    'sort': "newest",
-    //True or false, highlights search paramater
-    'hl': "true"
-});
+
+var generatedURL = []
+var search;
+var beginDate;
+var endDate;
+var sort;
+var highlight;
+
+function generateURL(){
+    
+}
+
+
+// url += '?' + $.param({
+//     //Standard API key
+//     'api-key': "3af329218907494292b4799b9106fbdd",
+//     //Search term
+//     'q': "Net neutrality",
+//     //Dates of search, format YYYYMMDD
+//     'begin_date': "20170101",
+//     'end_date': "20170601",
+//     //sort method, string, newest or oldest
+//     'sort': "newest",
+//     //True or false, highlights search paramater
+//     'hl': "true"
+// });
 $.ajax({
     url: url,
     method: 'GET',
